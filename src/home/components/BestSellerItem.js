1722e1b117d4a5b597eb898b52d660a7a37821f6
NPNKhoa <khoa.nphucnguyen@gmail.com>
2024-03-05 20:58:00 +0700

    xong trang chủ, trang đăng nhập, trang đăng ký

--- src/home/components/BestSellerItem.js
+++ src/home/components/BestSellerItem.js
@@ -9,17 +9,21 @@ const BestSellerItem = ({ ProductNo }) => {
 
     return (
         <div className='BestSellerItem'>
-            <div className='product-image'>
-                <img src={product.ProductImage}></img>
+            <div className='product-top'>
+                <div className='product-image'>
+                    <img src={product.ProductImage}></img>
+                </div>
+                <div className='product-name'>{product.ProductName}</div>
+                <div className='product-description'>
+                    {product.ProductDescription}
+                </div>
             </div>
-            <div className='product-name'>{product.ProductName}</div>
-            <div className='product-description'>
-                {product.ProductDescription}
+            <div className='product-bot'>
+                <div className='product-vote'>
+                    <i class='fa-solid fa-star fa-sm'></i> {product.ProductVote}
+                </div>
+                <div className='product-price'>{product.ProductPrice}</div>
             </div>
-            <div className='product-vote'>
-                <i class='fa-solid fa-star'></i> {product.ProductVote}
-            </div>
-            <div className='product-price'>{product.ProductPrice}</div>
         </div>
     );
 };
---
+++ src/home/json/BrandData.js
@@ -0,0 +1,23 @@
+export const BrandData = [
+    {
+        BrandNo: "001",
+        BrandName: "CENTELLA",
+        BrandImage: process.env.PUBLIC_URL + "/images/brand001.png",
+        BrandDescription:
+            "SKIN1004 Madagascar siêu cấp ẩm, phục hồi cho những làn da khô rát, thiếu sức sống",
+    },
+    {
+        BrandNo: "002",
+        BrandName: "AESTURA",
+        BrandImage: process.env.PUBLIC_URL + "/images/brand002.png",
+        BrandDescription:
+            "AESTURA mang đến trải nghiệm bảo vệ làn da trước tác hại xấu của các tác nhân bên ngoài ",
+    },
+    {
+        BrandNo: "003",
+        BrandName: "ROUND LAB",
+        BrandImage: process.env.PUBLIC_URL + "/images/brand003.png",
+        BrandDescription:
+            "ROUND LAB luôn chứng minh được sự uy tín suốt thời gian qua với các sản phẩm đa dạng, chất lượng, an toàn tuyệt đối",
+    },
+];

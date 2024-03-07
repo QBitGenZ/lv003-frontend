2ddad872027246a3474f62cfdde20d1a2ae5f091
NPNKhoa <khoa.nphucnguyen@gmail.com>
2024-03-07 22:57:58 +0700

    hoàn thành trang sản phẩm

---
+++ src/product/ProductReviews.js
@@ -0,0 +1,39 @@
+const ProductReviews = () => {
+    return (
+        <div className='ProductReviews'>
+            <div className='reviews-title'>Đánh giá sản phẩm</div>
+            <div className='product-reviews-container'>
+                <div className='reviews-left-side'>
+                    <div className='left-side-title'>
+                        Đánh giá về sản phẩm này
+                    </div>
+                    <div className='left-side-statistics'>
+                        <img
+                            src={
+                                process.env.PUBLIC_URL +
+                                "/images/statistics.png"
+                            }></img>
+                    </div>
+                </div>
+                <div className='reviews-right-side'>
+                    <div className='right-side-star'>
+                        <img
+                            src={
+                                process.env.PUBLIC_URL +
+                                "/images/review_stars.png"
+                            }></img>
+                    </div>
+                    <div className='right-side-rate'>4.3 / 5</div>
+                    <div className='right-side-reviews-quantity'>
+                        (1735 đánh giá)
+                    </div>
+                    <div className='button right-side-review-btn'>
+                        Viết đánh giá
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProductReviews;

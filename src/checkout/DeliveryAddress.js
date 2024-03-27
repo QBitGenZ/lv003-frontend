bb670f97ebe46d89dd31dc930870d4741818ce38
NPNKhoa <khoa.nphucnguyen@gmail.com>
2024-03-27 20:58:33 +0700

    hoàn thành chức năng đặt hàng

--- src/checkout/DeliveryAddress.js
+++ src/checkout/DeliveryAddress.js
@@ -1,22 +1,58 @@
-const DeliveryAddress = ({ handleClickedEvent }) => {
+const DeliveryAddress = ({ handleClickedEvent, address, setAddress }) => {
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setAddress({ ...address, [name]: value });
+    };
+
     return (
         <div id='DeliveryAddress'>
             <div className='delivery-address-title'>Địa chỉ nhận hàng</div>
             <div className='delivery-address-form'>
-                <input type='text' placeholder='Tỉnh/Thành phố'></input>
-                <input type='text' placeholder='Quận/Huyện'></input>
-                <input type='text' placeholder='Phường/Xã'></input>
                 <input
+                    name='recipientName'
                     type='text'
-                    placeholder='Chi tiết (số nhà, đường...)'></input>
-                <input type='text' placeholder='Tên người nhận'></input>
-                <input type='text' placeholder='Số điện thoại'></input>
+                    placeholder='Tên người nhận'
+                    value={address.recipientName}
+                    onChange={handleChange}></input>
+                <input
+                    name='phoneNumber'
+                    type='phone'
+                    placeholder='Số điện thoại '
+                    value={address.phoneNumber}
+                    onChange={handleChange}></input>
+                <input
+                    name='provinceCity'
+                    type='text'
+                    placeholder='Tỉnh/Thành phố'
+                    value={address.provinceCity}
+                    onChange={handleChange}></input>
+                <input
+                    name='district'
+                    type='text'
+                    placeholder='Quận/Huyện'
+                    value={address.district}
+                    onChange={handleChange}></input>
+                <input
+                    name='ward'
+                    type='text'
+                    placeholder='Phường/Xã'
+                    value={address.ward}
+                    onChange={handleChange}></input>
+                <input
+                    name='detailAddress'
+                    type='text'
+                    placeholder='Chi tiết (số nhà, đường...)'
+                    value={address.detailAddress}
+                    onChange={handleChange}></input>
                 <div className='form-footer'>
                     <div className='default-address'>
                         <input
+                            name='isDefaultAddress'
                             id='choose-default-address'
                             type='checkbox'
-                            placeholder=''></input>
+                            placeholder=''
+                            checked={address.isDefaultAddress}
+                            onChange={handleChange}></input>
                         <label htmlFor='choose-default-address'>
                             Đặt làm địa chỉ mặc định
                         </label>

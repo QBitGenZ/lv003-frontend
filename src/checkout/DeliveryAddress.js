a80c2f43d60b34239711fcae9f99b83fafcfc04b
NPNKhoa <khoa.nphucnguyen@gmail.com>
2024-04-21 23:46:57 +0700

    fix bug gì đó k biết

--- src/checkout/DeliveryAddress.js
+++ src/checkout/DeliveryAddress.js
@@ -1,18 +1,19 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const DeliveryAddress = ({ handleClickedEvent, address, setAddress }) => {
     const navigate = useNavigate();
 
     const handleChange = (e) => {
         const { name, value, type, checked } = e.target;
-        const newValue = type === 'checkbox' ? checked : value;
+        const newValue = type === "checkbox" ? checked : value;
         setAddress({ ...address, [name]: newValue });
     };
-    
+
     const handleBackButtonClick = () => {
         navigate(-1); // Trở về trang trước đó
     };
+
     return (
         <div id='DeliveryAddress'>
             <div className='delivery-address-title'>Địa chỉ nhận hàng</div>
@@ -23,50 +24,56 @@ const DeliveryAddress = ({ handleClickedEvent, address, setAddress }) => {
                     placeholder='Tên người nhận'
                     value={address.recipientName}
                     onChange={handleChange}></input>
+
                 <input
                     name='phoneNumber'
                     type='phone'
                     placeholder='Số điện thoại '
                     value={address.phoneNumber}
                     onChange={handleChange}></input>
+
                 <input
                     name='provinceCity'
                     type='text'
                     placeholder='Tỉnh/Thành phố'
                     value={address.provinceCity}
                     onChange={handleChange}></input>
+
                 <input
                     name='district'
                     type='text'
                     placeholder='Quận/Huyện'
                     value={address.district}
                     onChange={handleChange}></input>
+
                 <input
                     name='ward'
                     type='text'
                     placeholder='Phường/Xã'
                     value={address.ward}
                     onChange={handleChange}></input>
+
                 <input
                     name='detailAddress'
                     type='text'
                     placeholder='Chi tiết (số nhà, đường...)'
                     value={address.detailAddress}
                     onChange={handleChange}></input>
-                    <div className='default-address'>
-                        <input
-                            name='isDefaultAddress'
-                            id='choose-default-address'
-                            type='checkbox'
-                            placeholder=''
-                            checked={address.isDefaultAddress}
-                            onChange={handleChange}></input>
-                        <label htmlFor='choose-default-address'>
-                            Đặt làm địa chỉ mặc định
-                        </label>
-                    </div>
+
+                <div className='default-address'>
+                    <input
+                        name='isDefaultAddress'
+                        id='choose-default-address'
+                        type='checkbox'
+                        placeholder=''
+                        checked={address.isDefaultAddress}
+                        onChange={handleChange}></input>
+                    <label htmlFor='choose-default-address'>
+                        Đặt làm địa chỉ mặc định
+                    </label>
+                </div>
                 <div className='form-footer'>
-                <button
+                    <button
                         className='button order-btn'
                         onClick={handleBackButtonClick}>
                         Trở lại
@@ -74,17 +81,22 @@ const DeliveryAddress = ({ handleClickedEvent, address, setAddress }) => {
                     <button
                         className='button order-btn'
                         onClick={() => {
-                            if (!address.recipientName || !address.phoneNumber || !address.provinceCity || !address.district || !address.ward || !address.detailAddress) {
-                                alert('Vui lòng nhập đầy đủ thông tin địa chỉ');
+                            if (
+                                !address.recipientName ||
+                                !address.phoneNumber ||
+                                !address.provinceCity ||
+                                !address.district ||
+                                !address.ward ||
+                                !address.detailAddress
+                            ) {
+                                alert("Vui lòng nhập đầy đủ thông tin địa chỉ");
                                 return;
                             }
-                            handleClickedEvent()
-                            }
-                        }>
+                            handleClickedEvent();
+                        }}>
                         Tiếp theo
                     </button>
                 </div>
-                
             </div>
         </div>
     );
--- src/register/index.js
+++ src/register/index.js
@@ -1,12 +1,23 @@
 import Header from "../common/Header";
 import RegisterBody from "./RegisterBody";
 import "./Register.css";
+import SearchBody from "../common/SearchBody";
+import { useState } from "react";
 
 const Register = () => {
+    const [isShowSearch, setIsShowSearch] = useState(false);
+
     return (
         <>
-            <Header></Header>
-            <RegisterBody></RegisterBody>
+            <Header
+                isShowSearch={isShowSearch}
+                setIsShowSearch={setIsShowSearch}
+            />
+            {isShowSearch ? (
+                <SearchBody setIsShowSearch={setIsShowSearch} />
+            ) : (
+                <RegisterBody></RegisterBody>
+            )}
         </>
     );
 };
